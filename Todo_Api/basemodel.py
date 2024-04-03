421765f74415ad956bc53218eed99244663ffd66
Fadahunsi-Paul <scandiclamar@gmail.com>
2024-04-03 04:29:44 -0700

    initial commit

---
+++ Todo_Api/basemodel.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TimeBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("created_at") #ascending order
+        abstract = True
---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class CustomUserModel(models.Model):
+    ...
