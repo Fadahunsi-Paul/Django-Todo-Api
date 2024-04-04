5c2562b42d7cd94930da49a9244e22ca3c2e37f3
Fadahunsi-Paul <scandiclamar@gmail.com>
2024-04-04 04:12:42 -0700

    testing empty email instances

--- accounts/tests/tests_models.py
+++ accounts/tests/tests_models.py
@@ -10,4 +10,12 @@ class TestModel(APITestCase):
         self.assertFalse(user.is_staff)
         self.assertEqual(user.email,'dafa@gmail.com')
 
-    
+    def test_str_method_with_email(self):
+        user = User(email='dafa@gmail.com') # create an email instance
+        outcome = user.__str__()
+        self.assertEqual(outcome,'dafa@gmail.com') #checks if the return value matches the expected
+
+    def test_str_method_without_email(self):
+        user = User()
+        outcome = user.__str__()
+        self.assertEqual(outcome, '') #checks if the returned value is an empty string
