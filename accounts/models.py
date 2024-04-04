e08d951277259e47409c8c912740e5455669b4c5
Fadahunsi-Paul <scandiclamar@gmail.com>
2024-04-03 23:48:07 -0700

    created User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from Todo_Api.basemodel import TimeBaseModel
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone 
+from .usermanager import CustomUserManager
 
-# Create your models here.
 
-class CustomUserModel(models.Model):
-    ...
+class User(AbstractUser):
+    # username_validator = UnicodeUsernameValidator()
+
+    # username = models.CharField(
+    #     _("username"),
+    #     max_length=150,
+    #     unique=True,
+    #     help_text=_(
+    #         "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+    #     ),
+    #     validators=[username_validator],
+    #     error_messages={
+    #         "unique": _("A user with that username already exists."),
+    #     },
+    # )
+    username = None 
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    email_verified = models.BooleanField(
+        _("email_verfied"),
+        default=True,
+        help_text=_(
+            "Designates whether this users email is verified. "
+        ),
+    )
+    date_joined = models.DateTimeField(_("Date Joined"),auto_now_add=True) 
+    last_joined = models.DateTimeField(_("Last Joined"), auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email if self.email else ""
+
+    
+    @property 
+    def token(self):
+        return ""
+
+
+
---
+++ accounts/usermanager.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError("The Email Must be set")
+        if not password:
+            raise ValueError(_("The password Must be provided"))
+        if len(password) < 8:
+            raise ValueError(_("Password should be more than 8 Characters"))
+        if not "@" and ".com" in email:
+            raise ValueError(_("Email Should Contain @ and .com"))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user 
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
