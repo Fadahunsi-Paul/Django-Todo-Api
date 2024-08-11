3d8cd7d7c878593115da620507ab6a7e3885a0bd
unknown <scandiclamar@gmail.com>
2024-08-11 03:45:17 +0000

    new

--- api/views.py
+++ api/views.py
@@ -29,5 +29,14 @@ class TodoListView(APIView):
         return Response(serializer,status=status.HTTP_200_OK) 
     
     def put(self,request,id):
-        todo = 
-        serializer = TodoSerializer
+        todo = Todo.objects.get(id=id)
+        serializer = TodoSerializer(todo,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,id):
+        Todo.objects.get(id=id).delete()
+        return Response(status=status.HTTP_200_OK)
