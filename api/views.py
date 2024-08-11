1d6e078881cf0a0c2da537238ba81322dfac734e
unknown <scandiclamar@gmail.com>
2024-08-11 03:41:58 +0000

    new

--- api/views.py
+++ api/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from .models import Todo
 from .serializer import TodoSerializer
-from rest_framework import status
+from rest_framework import status,generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
 class TodoApiView(APIView):
     def get(self,request):
-        serializer = TodoSerializer
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo)
+        return Response(serializer,status=status.HTTP_200_OK)
+
+    def post(self,request):
+        serializer = TodoSerializer(data =request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
-    def post(self):
-        
+class TodoListView(APIView):
+    def get(self,request,id):
+        try:
+            todo = Todo.objects.all(id=id)
+        except Todo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = TodoSerializer(todo)
+        return Response(serializer,status=status.HTTP_200_OK) 
+    
+    def put(self,request,id):
+        todo = 
+        serializer = TodoSerializer
